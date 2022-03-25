f7eb6ec49ffb749a7cc7776cc2ebe8c1cc0f0bfd
PrinceB <bertrandprince01@gmail.com>
2022-03-25 12:59:20 -0500

    added Functionalities

--- frontend/src/reducks/carts/operations.js
+++ frontend/src/reducks/carts/operations.js
@@ -1,6 +1,7 @@
+import { createRef } from "react";
 import API from "../../API";
 
-import { adddCartAction, increaseCartAction, decreaseCartAction, fetchCartAction} from "./action";
+import { adddCartAction, increaseCartAction, decreaseCartAction, fetchCartAction} from "./actions";
 
 const api = new API();
 const CARTS_KEY = 'CARTS_KEY';
@@ -11,9 +12,58 @@ export const fetchFromLocalStorage = () => {
         let carts = {};
         if (cartsJSON) {
             carts = JSON.parse(cartsJSON);
-        }
+        } 
         const subtotal = calculateSubtotal(carts);
         dispatch(fetchCartAction(carts, subtotal));
     }
 }
 
+export const adddCart = (item) => {
+    return async (dispatch, getState) => {
+        let prevCarts = getState().carts.list
+        prevCarts[item.id]={item:item, selected_count:1};
+        const subtotal= calculateSubtotal(prevCarts);
+        setToLocalStorage(prevCarts);
+        dispatch(adddCartAction(prevCarts, subtotal));
+    }
+}
+
+export const increaseCart = (item) => {
+    return async (dispatch, getState) => {
+        let prevCarts= getState().carts.list
+        let nextSelectedCount = prevCarts[item.id].selected_count + 1;
+        prevCarts[item.id] = {"item":item, selected_count:nextSelectedCount};
+        const subtotal= calculateSubtotal(prevCarts);
+        setToLocalStorage(prevCarts);
+        dispatch(increaseCartAction(prevCarts, subtotal));
+    }
+}
+
+export const decreaseCart = (item) => {
+    return async (dispatch, getState) => {
+        let prevCarts=getState().carts.list
+        let nextSelectedCount = prevCarts[item.id].selected_count -1;
+        if (nextSelectedCount > 0) {
+            prevCarts[item.id] = {"item": item, "selected_count": nextSelectedCount};
+        } else {
+            delete prevCarts[item.id];
+        }
+        prevCarts[item.id] = {"item":item, selected_count:nextSelectedCount};
+        const subtotal = calculateSubtotal(prevCarts);
+        setToLocalStorage(prevCarts);
+        dispatch(decreaseCartAction(prevCarts, subtotal));
+    }
+}
+
+const setToLocalStorage=(carts) => {
+    localStorage.setItem(CARTS_KEY, JSON.stringify(carts));
+}
+
+const calculateSubtotal = (carts) => {
+    let subtotal = 0;
+    console.log(carts);
+    for (let key in carts) {
+        subtotal += (Number(carts[key].item.price)* carts[key].selected_count);
+    }
+    return subtotal
+}
